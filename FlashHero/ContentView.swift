21fcbcdd3887a21e85acfd37f1e7b161ac88cc6d
Emir Arı <emirariomatic@gmail.com>
2025-11-05 13:31:51 +0300

    Implement simple text recognition

--- FlashHero/ContentView.swift
+++ FlashHero/ContentView.swift
@@ -6,16 +6,59 @@
 //
 
 import SwiftUI
+import PhotosUI
+import Vision
 
 struct ContentView: View {
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var pickerUiImage: UIImage?
+    @State private var pickerImage: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PhotosPicker("Select photo", selection: $pickerItem, matching: .images)
+            
+            pickerImage?
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 200, height: 300)
         }
-        .padding()
+        .onChange(of: pickerItem) {
+            Task {
+                if let loadedImageData = try? await pickerItem?.loadTransferable(type: Data.self) {
+                    pickerUiImage = UIImage(data: loadedImageData)
+                    pickerImage = Image(uiImage: pickerUiImage!)
+                    
+                    recognizeText(from: pickerUiImage!)
+                } else {
+                    print("Failed to download image.")
+                }
+            }
+        }
+    }
+    
+    func recognizeText(from uiImage: UIImage) {
+        guard let cgImage = uiImage.cgImage else { return }
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+        
+        do {
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+    }
+    
+    func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        
+        let recognizedStrings = observations.compactMap { observation in
+            return observation.topCandidates(1).first?.string
+        }
+        
+        print(recognizedStrings)
     }
 }
 
