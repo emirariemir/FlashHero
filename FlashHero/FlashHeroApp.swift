78e0b55ee1af57956575118359cfc9f049a37304
Emir Arı <emirariomatic@gmail.com>
2025-11-05 12:55:56 +0300

    Initial Commit

---
+++ FlashHero/FlashHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashHeroApp.swift
+//  FlashHero
+//
+//  Created by Emir Arı on 5.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashHeroTests/FlashHeroTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlashHeroTests.swift
+//  FlashHeroTests
+//
+//  Created by Emir Arı on 5.11.2025.
+//
+
+import Testing
+
+struct FlashHeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
